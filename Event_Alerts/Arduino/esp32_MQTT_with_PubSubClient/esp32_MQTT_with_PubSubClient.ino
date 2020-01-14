7e0232ce2cbf5b5343e986c16fa86cdc6ebb2306
rurentero <46007399+rurentero@users.noreply.github.com>
2020-01-14 10:55:04 +0100

    Añadido: Selector de recursos
    Añadido selector en función del tipo de recurso recibido.
    Añadido métodos para las distintas llamadas de la API.
    Añadido método para mostrar notificaciones por la pantalla LCD.

--- Event_Alerts/Arduino/esp32_MQTT_with_PubSubClient/esp32_MQTT_with_PubSubClient.ino
+++ Event_Alerts/Arduino/esp32_MQTT_with_PubSubClient/esp32_MQTT_with_PubSubClient.ino
@@ -1,8 +1,10 @@
-// Sketch con el Public/Subscribe funcionando a 10/01
-// 
+// Sketch con el Public/Subscribe funcionando a 14/01
+// Sketch designed for M5-Stick-C based on Esp32 boards.
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <M5StickC.h>
+#include <ArduinoJson.h>
+// WARNING: MQTT_MAX_PACKET_SIZE needs to be defined in PubSubClient.h library. MQTT Max message size is 128 by default.
 
 const char* ssid = "Gloin";
 const char* password = "Gloin2014";
@@ -47,21 +49,12 @@ void setup() {
   // Subscribe to topic
   client.subscribe("mytopic/test");
 
-  // initialize the M5StickObject (for LCD notifications)
+  // Initialize the M5StickObject (for LCD notifications)
   M5.begin();
 }
 
 // ---- Function invoked every time a new message arrives to the topic. 
 void callback(char* topic, byte* payload, unsigned int length) {
-
-  // Show notification in LCD
-  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setCursor(0, 10);
-  M5.Lcd.setTextColor(WHITE);
-  M5.Lcd.setTextSize(1);
-  M5.Lcd.printf("New Notification!");
-  M5.Lcd.fillCircle(40, 80, 30, RED);
-
   // PubSub Tasks
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
@@ -70,13 +63,69 @@ void callback(char* topic, byte* payload, unsigned int length) {
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
- 
   Serial.println();
+
+  //Deserialize Json
+  Serial.println("Deserializating JSON");
+  DynamicJsonDocument doc(length);
+  DeserializationError err = deserializeJson(doc, payload, length);
+  if (err) {
+    Serial.print(F("deserializeJson() failed with code "));
+    Serial.println(err.c_str());
+  }
+  Serial.print(F("Recource Type: "));
+  const char* resource = doc["resource"];
+  Serial.println(resource);
+// TODO Probar resto de accesos a obetos/arrays anidados
+//  Serial.println(F("Params -> Event: "));
+//  Serial.println(doc["params"]["event"]["id"]);
+//  Serial.println(doc["params"]["event"]["title"]);
+//  Serial.println(doc["params"]["event"]["description"]);
+//  Serial.println(F("Params -> Event --> Location: "));
+//  Serial.println(doc["params"]["event"]["location"]["latitude"]);
+//  Serial.println(doc["params"]["event"]["location"]["longitude"]);
+//  Serial.println(doc["params"]["event"]["location"]["radius"]);
+
+  //Resource selector. TODO: En caso de APIs diferentes métodos para una ruta, habría que hacer un selector para el tipo de método
+  if (strcmp(resource,"Event")==0) {
+    notify("New notification: Event");
+    postEvent();
+  }
+  else if (strcmp(resource,"User")==0) {
+    notify("New notification: User");
+    getUsers();
+  }
+  else {  //Default
+    Serial.println("API resource not supported.");
+  }
+
   Serial.println("-----------------------");
- 
 }
 
 void loop() {
-  //client.publish("mytopic/test", "This is a message sent 5 seconds later");
   client.loop();
+}
+
+
+// ---- API Methods
+void postEvent() {
+  // Mocked response
+  Serial.println("Method: postEvent");
+}
+
+void getUsers() {
+  // Mocked response
+  Serial.println("Method: getUsers");
+}
+
+
+// ---- Utilities
+void notify(char* msg){
+  // Show notification in LCD. M5 must be initialized.
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 10);
+  M5.Lcd.setTextColor(WHITE);
+  M5.Lcd.setTextSize(1);
+  M5.Lcd.printf(msg);
+  M5.Lcd.fillCircle(40, 80, 30, RED);
 }
